a8d6945c184c0ac8a45c986ce08031cb0f777893
ezequiel huma brito <ezequielhuma@hotmail.com>
2019-06-21 12:14:30 -0400

    Creacion de las entidades

---
+++ ProyectoPatronRepositorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProyectoPatronRepositorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
